86013674881ebbec7190c97205ce056a86097460
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2022-12-30 10:19:19 +0100

    burger menu and moving translation implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   marker: string = 'home';
+  open: boolean = false;
+  icon_src: string = "../../assets/img/burger.png";
 
   constructor() { }
 
@@ -18,6 +20,15 @@ export class HeaderComponent implements OnInit {
   scrollTo(anchor: string) {
     this.marker = anchor;
     console.log(this.marker);
+  }
+
+  toggleMenu() {
+    let icons = document.querySelectorAll('.menu-icon');
+    icons.forEach((icon) => {
+      icon.classList.toggle('open');
+    })
+
+    document.querySelector('.mobile-menu').classList.toggle('move');
   }
 
 }
