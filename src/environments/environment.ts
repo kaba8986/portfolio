8683f7913a1057868883167acb8646c1aef7d33e
Andreas Burghardt <109896410+kaba8986@users.noreply.github.com>
2023-01-02 11:27:07 +0100

    checked up all personal texts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyB0gTvb_7vUVdiaSdZR8P68qKHn1JA-Wpo",
+  authDomain: "portfolio-6db3b.firebaseapp.com",
+  projectId: "portfolio-6db3b",
+  storageBucket: "portfolio-6db3b.appspot.com",
+  messagingSenderId: "366009498762",
+  appId: "1:366009498762:web:2aeb30f8ce45bb5b1c4173"
+
 };
 
 /*
