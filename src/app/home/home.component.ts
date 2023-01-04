5cf7b066080768ac45ac91641075f95dcd00ac65
Andreas Burghardt <andreas-burghardt@gmx.net>
2023-01-04 12:54:42 +0100

    responsive fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,14 +7,18 @@ import { Component, OnInit} from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   opacVal = 1;
-  
+  x = window.matchMedia("(max-width: 1100px)");
 
   constructor() { 
     window.addEventListener('scroll' , () => {
-      let value = 1 + window.scrollY/ -420;
+      let value;
+      if(this.x.matches) {
+        value = 2.5 + window.scrollY/ -300;
+      } else {
+        value = 1 + window.scrollY/ -420;
+      }
       this.opacVal = value;
       console.log(this.opacVal);
-      
     })
   }
 
