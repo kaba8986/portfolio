8a001e025b27c39e400a6d038ee7b61915fed39b
Andreas <andreas-burghardt@gmx.net>
2026-06-23 11:01:32 +0200

    ng update @angular/core@21 @angular/cli@21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -6,5 +6,5 @@ import { environment } from './environments/environment';
 
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
