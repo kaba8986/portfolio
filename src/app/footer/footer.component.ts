e52ca0e7aac9b6e2dd60e825f37f60e3ffbfc4a9
Andreas <andreas-burghardt@gmx.net>
2026-06-23 15:25:39 +0200

    refactored footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,6 +6,11 @@ import {RouterLink} from '@angular/router';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   imports: [RouterLink],
-  standalone: true,
+  standalone: true
 })
-export class FooterComponent  {}
+export class FooterComponent  {
+  readonly githubLink = 'https://github.com/kaba8986';
+  readonly linkedinLink = 'https://www.linkedin.com/in/andreas-burghardt-743563260/';
+  readonly xingLink = 'https://www.xing.com/profile/Andreas_Burghardt21';
+  readonly copyRight = '© 2023 Andreas Burghardt';
+}
