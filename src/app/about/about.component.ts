940b2227972873261ebeaac0005c2e516fbccfb8
Andreas <andreas-burghardt@gmx.net>
2026-06-23 15:05:42 +0200

    replaced pics

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
+import {LucideLightbulb, LucidePuzzle} from "@lucide/angular";
 
 @Component({
   selector: 'ab-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+  imports: [
+    LucideLightbulb,
+    LucidePuzzle
+  ],
   standalone: true
 })
 export class AboutComponent {}
